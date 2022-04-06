3301e461fcc5a898248a1816276d6361e8a65777
user <user@DESKTOP-26DH5CC>
2022-04-06 16:02:42 -0300

    primer Commit

---
+++ ProjectDemo/src/test/java/ar/edu/unju/fi/pvisual/aplicacion/ejemplos/ProjectDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.pvisual.aplicacion.ejemplos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
